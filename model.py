56c9c11d7dd883d9d8be1dc615feacc279bffad2
AirrowSST <airrowsst@gmail.com>
2023-09-02 01:29:05 +0800

    repr for Organization, fixed IPAddressBlock error

--- model.py
+++ model.py
@@ -100,13 +100,13 @@ class IPAddressBlock:
         return self.ip_address.get_copy()
 
     def get_broadcast_address(self) -> 'IPAddress':
-        return self.ip_address + self.get_num_usable_addresses()
+        return self.ip_address + self.get_num_usable_addresses() - 1
     
     def get_lower_bound_address(self) -> 'IPAddress':
         return self.ip_address + 1
     
     def get_upper_bound_address(self) -> 'IPAddress':
-        return self.ip_address + self.get_num_usable_addresses() - 1
+        return self.ip_address + self.get_num_usable_addresses() - 2
     
     def get_num_usable_addresses(self) -> int:
         return 2 ** (32 - self.ip_address.subnet_mask_length)
@@ -127,15 +127,18 @@ class IPAddressBlock:
             return self.current_address - 1
     
     def __repr__(self):
-        return "Address Block: " + str(self.get_identity_address()) + " (Network ID) - " + str(self.get_broadcast_address())
+        return "Address Block: " + str(self.get_identity_address()) + " (ID) - " + str(self.get_broadcast_address())
     
 class Organization:
-    def __init__(self, name, ip_address_blocks: list[IPAddressBlock] = None):
-        self.name = name
-        self.ip_address_blocks = ip_address_blocks if ip_address_blocks is not None else []
+    def __init__(self, name: str, ip_address_blocks: list[IPAddressBlock] = None):
+        self.name: str = name
+        self.ip_address_blocks: list[IPAddressBlock] = list(ip_address_blocks) if ip_address_blocks is not None else []
     
     def total_usable_ip_addresses(self):
         return sum([block.get_num_usable_addresses() for block in self.ip_address_blocks])
+    
+    def __repr__(self):
+        return self.name + ": \n" + "\n".join([str(ip_address_block) for ip_address_block in self.ip_address_blocks])
     
 class Database:
     def __init__(self):
