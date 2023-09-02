8415f10cc9ab777eeaefe8e31374f88ea8019117
AirrowSST <airrowsst@gmail.com>
2023-09-02 09:42:50 +0800

    added application class with GUI

---
+++ application.py
@@ -0,0 +1,25 @@
+import customtkinter as ctk
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("my app")
+        self.geometry("400x150")
+        self.grid_columnconfigure((0, 1), weight=1)
+
+        self.button = ctk.CTkButton(self, text="my button", command=self.button_callback)
+        self.button.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+        self.checkbox_1 = ctk.CTkCheckBox(self, text="checkbox 1")
+        self.checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
+        self.checkbox_2 = ctk.CTkCheckBox(self, text="checkbox 2")
+        self.checkbox_2.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+        
+    def button_callback(self):
+        print("button pressed")
+
+ctk.set_appearance_mode("System")  # Modes: system (default), light, dark
+ctk.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+
+app = App()
+app.mainloop()
